4f859d041bb7616dbf7d29e50cf0ecf0b3ceb2f6
vinayreddy-bulagakula <157707465+vinayreddy-bulagakula@users.noreply.github.com>
2025-09-03 13:01:37 +0100

    Add files via upload

---
+++ backend/db/connection.py
@@ -0,0 +1,11 @@
+
+import pymysql
+
+def get_db_connection():
+    return pymysql.connect(
+        host="localhost",
+        port=3307,
+        user="root",
+        password="root123", 
+        database="zta_security"
+    )
